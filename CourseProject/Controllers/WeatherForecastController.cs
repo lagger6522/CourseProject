8ac629066ec14175c744895f6be44429d7d941c8
Ballamut * <shulyak.1975@bk.ru>
2024-01-04 23:58:25 +0300

    first commit

---
+++ CourseProject/Controllers/WeatherForecastController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProject.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+	};
+
+		private readonly ILogger<WeatherForecastController> _logger;
+
+	}
+}
