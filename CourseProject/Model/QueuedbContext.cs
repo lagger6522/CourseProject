21109f256dfdfd1b420decba845a5bf59d4fd8a7
SwanPam <alexei.matsukevitch@yandex.by>
2024-01-05 20:56:38 +0300

    Настроена локальная БД + работает регистрация/авторизация

--- CourseProject/Model/QueuedbContext.cs
+++ CourseProject/Model/QueuedbContext.cs
@@ -18,8 +18,7 @@ public partial class QueuedbContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=192.168.100.166,1433;Initial Catalog=QUEUEDB;User ID=sa;Password=123;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-IHVPCAN\\POLYCLINICS;Initial Catalog=QUEUEDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
