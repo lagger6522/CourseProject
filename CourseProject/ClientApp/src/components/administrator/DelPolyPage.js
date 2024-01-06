4a2f047d7c90409903896d8c099fc39f8ff62c5f
SwanPam <alexei.matsukevitch@yandex.by>
2024-01-06 04:10:51 +0300

    Добавлена минимальная форма удаления поликлиники

--- CourseProject/ClientApp/src/components/administrator/DelPolyPage.js
+++ CourseProject/ClientApp/src/components/administrator/DelPolyPage.js
@@ -12,8 +12,18 @@ export class DelPolyPage extends Component {
 
     render() {
         return (
-            <div>
-                Hello, i'm DelPolyPage!
+            <div className="form-container">
+                <h2>Удаление поликлиники</h2>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="clinicName">Наименование клиники</label>
+                        <input type="text" id="clinicName" name="clinicName" required />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Удалить" style={{ width: '100%' }} />
+                    </div>
+                </form>
+                <button className="admin-corner-button">&#8606;</button>
             </div>
         );
     }
